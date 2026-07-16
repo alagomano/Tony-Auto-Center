49ed014c6ae3fd4e4ec2102665f6d3c905ca1f10
alagomano <nicolas.brayan.silva@gmail.com>
2026-07-16 17:52:01 -0300

    feat: implementa entidade Client com CPF, telefone e-mail e endereço

---
+++ src/model/entities/Client.java
@@ -0,0 +1,54 @@
+package model.entities;
+
+import model.exception.ServiceException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Client {
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private String phone;
+    private String address;
+    private Map<String, Vehicle> vehicles = new HashMap<>();
+
+    public Client(String name, String cpf, String phone, String address){
+        this.name = name;
+        this.cpf = cpf;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void addVehicle(Vehicle vehicle){
+        if(vehicle == null){
+            throw new ServiceException("Veículo inválido!");
+        }
+        if(vehicles.containsKey(vehicle.getPlate())){
+            throw new ServiceException("Veículo já cadastrado");
+        }
+        vehicles.put(vehicle.getPlate(), vehicle);
+    }
+
+    public Collection<Vehicle> getVehicles(){
+        return new ArrayList<>(vehicles.values());
+    }
+
+    public void removeVehicle(String plate){
+        if(!vehicles.containsKey(plate)) {
+            throw new ServiceException("Veículo não encontrado");
+        }
+
+        vehicles.remove(plate);
+    }
+
+
+
+}
