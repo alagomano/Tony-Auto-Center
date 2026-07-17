ada74c66254f3ac159ce51a84a187dcb9e8c3bc5
alagomano <nicolas.brayan.silva@gmail.com>
2026-07-17 16:42:12 -0300

    feat: implementa ClientService com registro, busca e remoção por CPF

---
+++ src/model/services/ClientService.java
@@ -0,0 +1,44 @@
+package model.services;
+
+import model.entities.Client;
+import model.exception.ServiceException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClientService {
+
+    private Map<String, Client> clients = new HashMap<>();
+
+    public void registerClient(Client client){
+        if(clients.containsKey(client.getCpf())){
+            throw new ServiceException("Cliente já existe.");
+        }
+
+        clients.put(client.getCpf(), client);
+    }
+
+    public Client fundClients(String cpf){
+        Client client = clients.get(cpf);
+        if(client == null){
+            throw new ServiceException("Cliente não encontrado");
+        }
+        return client;
+    }
+
+    public void removeClient(Client client){
+        if(!clients.containsKey(client.getCpf())){
+            throw new ServiceException("Cliente não se encontra da lista.");
+        }
+
+        clients.remove(client.getCpf());
+
+    }
+
+    public Collection<Client> getClients(){
+        return new ArrayList<>(clients.values());
+    }
+
+}
