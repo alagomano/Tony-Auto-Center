c7214c4ec5417d94d8eeec7a4bdacf3c301f4b7d
alagomano <nicolas.brayan.silva@gmail.com>
2026-07-24 14:14:02 -0300

    refactor: melhora tratamento de exceções na DbException

--- src/main/java/model/exception/DbException.java
+++ src/main/java/model/exception/DbException.java
@@ -4,4 +4,8 @@ public class DbException extends RuntimeException{
     public DbException(String msg){
         super(msg);
     }
+
+    public DbException(String msg, Throwable cause){
+        super(msg, cause);
+    }
 }
