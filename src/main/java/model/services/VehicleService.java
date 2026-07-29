1e1dc1475faa5bb473f1b0f20627dbb65e54c9f7
alagomano <nicolas.brayan.silva@gmail.com>
2026-07-28 21:33:16 -0300

    feat: implementa VehicleService

--- src/main/java/model/services/VehicleService.java
+++ src/main/java/model/services/VehicleService.java
@@ -1,44 +1,49 @@
 package model.services;
 
+import model.dao.DaoFactory;
+import model.dao.ServiceOrderDao;
+import model.dao.VehicleDao;
 import model.entities.Client;
-import model.entities.ServiceOrder;
 import model.entities.Vehicle;
-import model.enums.OrderStatus;
 import model.exception.ServiceException;
 
 public class VehicleService {
 
     private final ClientService clientService;
+    private final VehicleDao vehicleDao = DaoFactory.createVehicleDao();
+    private final ServiceOrderDao serviceOrderDao = DaoFactory.createServiceOrderDao();
 
     public VehicleService(ClientService clientService){
         this.clientService = clientService;
     }
 
-    public void registerVehicle(String cpf, Vehicle vehicle){
-        Client client = clientService.findClient(cpf);
-        client.addVehicle(vehicle);
+    private void validateVehicle(Vehicle vehicle){
+        if(vehicle == null){
+            throw new ServiceException("Veículo inválido.");
+        }
     }
 
-    public Vehicle findVehicle(String cpf, String plate){
-        Client client = clientService.findClient(cpf);
-        Vehicle vehicle = client.getVehicle(plate);
-        if (vehicle == null){
-            throw new ServiceException("Veículo não encontrado.");
+    private void validatePlate(String plate){
+        if (plate == null || plate.isBlank()){
+            throw new ServiceException("Placa inválida.");
         }
+    }
+
+    public void registerVehicle(String cpf, Vehicle vehicle){
+        validateVehicle(vehicle);
+        Client client = clientService.findClient(cpf);
+        vehicle.setClient(client);
+        vehicleDao.insert(vehicle);
+    }
+
+    public Vehicle findVehicle(String plate){
+        validatePlate(plate);
+        Vehicle vehicle = vehicleDao.findByPlate(plate);
+        validateVehicle(vehicle);
+
         return vehicle;
     }
 
-    public ServiceOrder openServiceOrder(String cpf, String plate, String description, String observation){
-        Vehicle vehicle = findVehicle(cpf, plate);
 
-        boolean verificationOS = vehicle.getHistory().stream().anyMatch(os -> os.getStatus() == OrderStatus.OPEN || os.getStatus() == OrderStatus.IN_PROGRESS);
-
-        if (verificationOS){
-            throw new ServiceException("Veículo já possui uma ordem de serviço ativa.");
-        }
-
-        return vehicle.openServiceOrder(description, observation);
-
-    }
 
 }
