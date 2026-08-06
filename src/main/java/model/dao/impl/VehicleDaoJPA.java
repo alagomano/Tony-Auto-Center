b9c870c45dd6accd9f97d57bdadba19f218aa0e2
alagomano <nicolas.brayan.silva@gmail.com>
2026-08-06 17:29:50 -0300

    feat: implementa VehicleDao com JPA

--- src/main/java/model/dao/impl/VehicleDaoJPA.java
+++ src/main/java/model/dao/impl/VehicleDaoJPA.java
@@ -1,8 +1,12 @@
 package model.dao.impl;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.PersistenceException;
 import model.dao.VehicleDao;
 import model.entities.Vehicle;
+import model.exception.DbException;
 
 import java.util.List;
 
@@ -13,37 +17,103 @@ public class VehicleDaoJPA implements VehicleDao {
     }
 
     @Override
-    public void insert(Vehicle entity) {
-
+    public void insert(Vehicle vehicle) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            entityManager.persist(vehicle);
+            transaction.commit();
+        }catch (PersistenceException e){
+            if(transaction.isActive()){
+                transaction.rollback();
+            }
+            throw new DbException("Erro ao inserir Veículo.", e);
+        }
     }
 
     @Override
-    public void update(Vehicle entity) {
-
+    public void update(Vehicle vehicle) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            entityManager.merge(vehicle);
+            transaction.commit();
+        }catch (PersistenceException e){
+            if(transaction.isActive()){
+                transaction.rollback();
+            }
+            throw new DbException("Erro ao atualizar informações do veículo.", e);
+        }
     }
 
     @Override
-    public void deleteById(Long aLong) {
+    public void deleteById(Long id) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            Vehicle vehicle = entityManager.find(Vehicle.class, id);
+            if (vehicle == null) {
+                throw new DbException("Veículo não encontrado");
+            }
 
+            transaction.begin();
+            entityManager.remove(vehicle);
+            transaction.commit();
+        }catch (PersistenceException e){
+            if(transaction.isActive()){
+                transaction.rollback();
+            }
+            throw new DbException("Erro ao deletar veículo.", e);
+        }
     }
 
     @Override
-    public Vehicle findById(Long aLong) {
-        return null;
+    public Vehicle findById(Long id) {
+        try {
+            return entityManager.find(Vehicle.class, id);
+        }catch (PersistenceException e){
+            throw new DbException("Erro ao buscar veículo.", e);
+        }
     }
 
     @Override
     public List<Vehicle> findAll() {
-        return null;
+        try {
+            String jpql = """
+                    SELECT v FROM Vehicle v
+                    """;
+            return entityManager.createQuery(jpql, Vehicle.class).getResultList();
+        }catch (PersistenceException e){
+            throw new DbException("Erro ao listar veículos.", e);
+        }
     }
 
     @Override
     public Vehicle findByPlate(String plate) {
-        return null;
+        try {
+            String jpql = """
+                    SELECT v FROM Vehicle v
+                    WHERE v.plate = :plate
+                    """;
+            return entityManager.createQuery(jpql, Vehicle.class).setParameter("plate", plate).getSingleResult();
+
+        }catch (NoResultException e){
+            return null;
+        }catch (PersistenceException e){
+            throw new DbException("Erro ao buscar veículo por placa.", e);
+        }
     }
 
     @Override
     public List<Vehicle> findByClient(Long clientId) {
-        return null;
+        try {
+            String jpql = """
+                    SELECT v FROM Vehicle v
+                    WHERE v.client.id = :id
+                    """;
+            return entityManager.createQuery(jpql, Vehicle.class).setParameter("id", clientId).getResultList();
+
+        }catch (PersistenceException e){
+            throw new DbException("Erro ao buscar veículos do cliente.", e);
+        }
     }
 }
