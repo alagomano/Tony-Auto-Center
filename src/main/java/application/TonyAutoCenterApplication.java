225f7ebd8e3ad95a4148ff6ed0c0a09539e40451
alagomano <nicolas.brayan.silva@gmail.com>
2026-08-14 00:44:45 -0300

    config: configura varredura dos repositories

--- src/main/java/application/TonyAutoCenterApplication.java
+++ src/main/java/application/TonyAutoCenterApplication.java
@@ -3,12 +3,14 @@ package application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
         "application",
         "model"
 })
 @EntityScan("model.entities")
+@EnableJpaRepositories(basePackages = "model.repositories")
 public class TonyAutoCenterApplication {
 
     public static void main(String[] args) {
