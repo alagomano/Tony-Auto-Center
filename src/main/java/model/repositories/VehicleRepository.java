6cdec4e41b37ab43204834da556cb2ec6d54fa18
alagomano <nicolas.brayan.silva@gmail.com>
2026-08-20 17:55:51 -0300

    feat: adiciona busca de veículo por placa

--- src/main/java/model/repositories/VehicleRepository.java
+++ src/main/java/model/repositories/VehicleRepository.java
@@ -3,5 +3,8 @@ package model.repositories;
 import model.entities.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    Optional<Vehicle> findByPlate(String plate);
 }
